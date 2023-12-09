88ac2fcad5e333bdd9becef1cba270f37f2bcae1
lesley_w <lrwesley92@gmail.com>
2023-12-09 15:55:32 -0600

    Added Header component and base styling for body and app

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Header () {
+  return (
+    <header>
+        <img src="../src/assets/icon-star.svg" alt="" className="star-icon"/>
+        <h1>FAQs</h1>
+    </header>
+  )
+}
+
+export default Header
