10b0a9a206853f4b3227023d6b219aac16a1e243
lesley_w <lrwesley92@gmail.com>
2023-12-10 17:53:25 -0600

    Added styling for questions

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from "./components/Header.jsx"
+import QuestionSection from "./components/QuestionSection.jsx"
 
 function App() {
  return (
    <div className="app">
       <Header />
+      <QuestionSection />
    </div>
  )
 }
---
+++ src/components/QuestionSection.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+function QuestionSection() {
+  return (
+    <div className="question-section-container">
+        <div className="single-question">
+            <div className="question-heading">
+                <h2>What is Frontend Mentor, and how will it help me?</h2>
+                <button type="button"></button>
+            </div>
+            <p className="answer">
+              Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default QuestionSection
